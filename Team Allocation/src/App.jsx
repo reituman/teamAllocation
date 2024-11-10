6a4b8f4e8b7b0fffbd66230aa5857f2150aa1587
reituman <reituman@gmail.com>
2024-11-10 12:08:10 +0200

    Simple UI

---
+++ Team Allocation/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Employees from "./layout/Employees";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Employees />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
